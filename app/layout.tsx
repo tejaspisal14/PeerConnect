0830a2fd3c261693413f4d7a1188231f48afb617
HoneyWadwa <hanuwadhwa55@gmail.com>
2025-09-22 18:03:43 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'PeerConnect',
+  description: 'Ai-Powered Peer Learning Platform',
   generator: 'v0.app',
 }
 
